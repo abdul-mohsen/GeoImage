22e305cd61ef6a87519d3c9ab55a991f9cb7e3a3
Mohsen(ssda) <abdulmohsen.almajed@tuwaiq.edu.sa>
2020-11-30 10:29:25 +0300

    Infalted the fragment and added it to the nav view

--- app/src/main/java/com/bignerdranch/android/geoimage/control/ImageList.kt
+++ app/src/main/java/com/bignerdranch/android/geoimage/control/ImageList.kt
@@ -1,6 +1,27 @@
 package com.bignerdranch.android.geoimage.control
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bignerdranch.android.geoimage.databinding.FragmentImageListBinding
 
 class ImageList: Fragment() {
+    private lateinit var binding: FragmentImageListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentImageListBinding.inflate(inflater, container,false)
+        setupUI()
+        return binding.root
+    }
+
+    private fun setupUI() {
+
+    }
 }
