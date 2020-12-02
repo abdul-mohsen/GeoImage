d89552d92d3e4fa50cc419ee5e6f2a3f5bc5c659
Mohsen(ssda) <abdulmohsen.almajed@tuwaiq.edu.sa>
2020-12-02 12:27:25 +0300

    Set the setting for picasso

--- app/src/main/java/com/bignerdranch/android/geoimage/App.kt
+++ app/src/main/java/com/bignerdranch/android/geoimage/App.kt
@@ -1,6 +1,11 @@
 package com.bignerdranch.android.geoimage
 
 import android.app.Application
+import com.squareup.picasso.Picasso
 
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Picasso.get().setIndicatorsEnabled(true)
+    }
 }
